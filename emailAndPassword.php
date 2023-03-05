fa74a470301fde72e5f0ab0da6d7ab3097964a58
HassanZeinAlDeen <hassanzeinaldeen0@gmail.com>
2023-03-06 00:34:35 +0200

    adding validate email and password

---
+++ emailAndPassword.php
@@ -0,0 +1,42 @@
+<?php
+$user_id= $_POST['Id'];
+$email = $_POST['Email'];
+$password = $_POST['Password'];
+$query = $mysqli->prepare('SELECT `Email`, `Password` FROM user where Id=?');
+$query->bind_param('i', $user_id);
+$query->execute();
+$query->store_result();
+$hashed_password = password_hash($password, PASSWORD_BCRYPT);
+
+$response ="";
+
+if(validEmail($email)){
+  if(validPassword($password)){
+    $response = "valid email and password";
+  }else{
+    $response = "invalid password";
+  }
+}else{
+  $response="email not valid";
+}
+
+function validEmail($email) {
+  if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validPassword($password){
+
+  if (preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{8,}$/", $password)) {
+      echo "Password format is valid";
+  } else {
+      echo "Password format is invalid";
+  }
+}
+
+
+echo json_encode($response);
+?>
